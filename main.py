6c80fb9479b67c79501b827ad3cd6dd374210167
Federico Dolce <psykopear@gmail.com>
2018-11-08 12:36:44 +0100

    Fixes

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import os
 import sys
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(
-        loader=FileSystemLoader(searchpath='./templates'),
+        loader=FileSystemLoader(searchpath='templates'),
         autoescape=select_autoescape(['html', 'txt'])
 )
-template = env.get_template(sys.argv[1])
-result = template.render()
-print(result)
-if '-w' in sys.argv:
-    with open('result.html', 'w') as f:
+for template_file in os.listdir('templates'):
+    template = env.get_template(template_file)
+    result = template.render()
+    with open('results/%s' % template.filename.split('/')[-1], 'w') as f:
         f.write(result)
 
